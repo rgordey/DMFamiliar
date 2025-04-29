edb896000d10445a527f534d9175b1dafbcdd4b4
Randy Gordey <Randy.Gordey@gmail.com>
2025-04-29 12:54:56 -0500

    Initial

---
+++ Domain/Character.cs
@@ -0,0 +1,7 @@
+﻿namespace Domain
+{
+    public class Character
+    {
+        public Guid Id { get; set; }
+    }
+}
---
+++ Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Domain;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
+            :base(options)
+        {
+            
+        }
+
+        public DbSet<Character> Characters { get; set; }
+    }
+}
